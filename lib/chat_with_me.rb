67390a15308ffecbe51daa673599d1a4ba20c1bd
Peter Muturi <pmuturi65@gmail.com>
2015-08-18 19:11:35 +0300

    gets name

--- lib/chat_with_me.rb
+++ lib/chat_with_me.rb
@@ -1,5 +1,9 @@
 require "chat_with_me/version"
 
-module ChatWithMe
-  # Your code goes here...
+module Chat
+    def self.Shirley
+    	puts "Hey There!!\n My name is Shirley and I'm dying to know you.\n Whats your name?:)"
+    	nm = gets.chomp!
+    	puts "Glad to meet you #{nm}" 
+  	end
 end
