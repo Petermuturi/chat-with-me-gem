b68d85413964a89bcbef5e22cf64103e5df2fa37
Peter Muturi <pmuturi65@gmail.com>
2015-08-18 19:21:50 +0300

    added age

--- lib/chat_with_me.rb
+++ lib/chat_with_me.rb
@@ -3,7 +3,10 @@ require "chat_with_me/version"
 module Chat
     def self.Shirley
     	puts "Hey There!!\n My name is Shirley and I'm dying to know you.\n Whats your name?:)"
-    	nm = gets.chomp!
+    	nm = gets.chomp
     	puts "Glad to meet you #{nm}" 
+    	puts "How old are you?"
+    	age = gets.chomp
+    	puts "interesting, I'm 19 years old.\n Oh and also I don't age;)"
   	end
 end
